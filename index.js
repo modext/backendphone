d0f565ac4f5ead526c7c16620c15fc0e8574b482
Your Name <33578391+modext@users.noreply.github.com>
2023-03-23 08:21:37 +0100

    changes

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const morgan = require("morgan");
+const cors = require("cors")
 
 const app = express ()
 
+
+app.use(cors())
 app.use(express.json())
 // app.use(morgan('tiny'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
@@ -92,7 +95,8 @@ app.post('/api/persons', (req, res)=>{
 })
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 
-const PORT = 3001;
+// const PORT = process.env.PORT || 3001
+const PORT =  3001
 app.listen(PORT, ()=>{
     console.log(`Server running on port ${PORT}`);
 });
